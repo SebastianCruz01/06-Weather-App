572da71529cc4b48b370e0fca5b6bf7b39dd3665
SebastianCruz01 <110071825+SebastianCruz01@users.noreply.github.com>
2022-10-02 13:15:11 -0400

    WEATHERAPP

---
+++ script.js
@@ -0,0 +1,10 @@
+$("#run-search").click(function(){
+
+    alert("clicked!");                      //function for search button click
+
+}); 
+$("#clear-search").click(function(){
+
+    alert("clicked!");                      //function for clear search button click
+
+});
