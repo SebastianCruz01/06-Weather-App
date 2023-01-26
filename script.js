d9734d13bd0bb09770e8cdffa60ca1a1d425a11f
SebastianCruz01 <110071825+SebastianCruz01@users.noreply.github.com>
2023-01-25 22:56:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,30 +17,30 @@ $("#previousSearch").click(function() {              // Previous Citys show unde
 })
 
 if (localStorage.getItem("localWeatherSearches")) {         // Local Storage functionality //
-    citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches"));
-    writeSearchHistory(citiesArray);
+    citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches")); // Get search history from local storage //
+    writeSearchHistory(citiesArray);                      // Write search history to local storage //
 } else {
     citiesArray = [];
 };
     // Call API for current weather by city name 
 function returnCurrentWeather(cityName) {
-    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${apiKey}`;
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${apiKey}`; 
 
     // Get info from URL When searched
-    $.get(queryURL).then(function(response){
-        // Current Date 
-        let currTime = new Date(response.dt*1000);
+    $.get(queryURL).then(function(response){ 
+        // Current Date  
+        let currTime = new Date(response.dt*1000);  
         // Displays Weather Icon 
-        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`; 
 
         // When searched display info on Weather info
         currWeatherDiv.html(`
         <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
-        <p>Temperature: ${response.main.temp}&#176;F</p>
-        <p>Humidity: ${response.main.humidity}%</p>
+        <p>Temperature: ${response.main.temp}&#176;F</p> 
+        <p>Humidity: ${response.main.humidity}%</p> 
         <p>Wind Speed: ${response.wind.speed} mph</p>
         `, returnUVIndex(response.coord))
-        createHistoryButton(response.name);
+        createHistoryButton(response.name); // Create history button for searched city
     })
 };
 
@@ -48,10 +48,10 @@ function returnCurrentWeather(cityName) {
 function returnWeatherForecast(cityName) {
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&APPID=6c0ac38b22e3e819b50460a5a899f855`;
 
-    $.get(queryURL).then(function(response){
-        let forecastInfo = response.list;
-        forecastDiv.empty();
-        $.each(forecastInfo, function(i) {
+    $.get(queryURL).then(function(response){ 
+        let forecastInfo = response.list;  // Forcast info
+        forecastDiv.empty();               // Empty forcast div
+        $.each(forecastInfo, function(i) { // Forcast info
             if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
                 return;
             }
@@ -60,14 +60,14 @@ function returnWeatherForecast(cityName) {
             let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`; // Displays Weather Icon 
             // Append data to 5 day forcast when searched 
             forecastDiv.append(`
-            <div class="col-md">
-                <div class="card text-white bg-primary">
+            <div class="col-md">     
+                <div class="card text-white bg-primary"> 
                     <div class="card-body">
-                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
-                        <img src=${weatherIcon} alt="Icon">
-                        <p>Temp: ${forecastInfo[i].main.temp}&#176;F</p>
-                        <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
-                    </div>
+                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4> 
+                        <img src=${weatherIcon} alt="Icon"> 
+                        <p>Temp: ${forecastInfo[i].main.temp}&#176;F</p> 
+                        <p>Humidity: ${forecastInfo[i].main.humidity}%</p> 
+                    </div> 
                 </div>
             </div>
             `)
@@ -78,7 +78,7 @@ function returnWeatherForecast(cityName) {
 function returnUVIndex(coordinates) {
     let queryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${coordinates.lat}&lon=${coordinates.lon}&APPID=${apiKey}`;
 
-    $.get(queryURL).then(function(response){
+    $.get(queryURL).then(function(response){ 
         let currUVIndex = response.value;
         let uvSeverity = "green";
         let textColour = "white"
